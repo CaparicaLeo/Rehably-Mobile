fcf442525f0e6b6ee69085cefb04862949145d38
CaparicaLeo <57023740005@unicentro.edu.br>
2026-06-15 14:23:26 -0300

    chore: use LAN IP instead of ngrok

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'https://semijuridical-crystal-nonreverently.ngrok-free.dev/api';
+  static const String baseUrl = 'http://172.20.10.4/api';
   static const String tokenKey = 'auth_token';
   static const String userKey = 'auth_user';
 }
